50a1f298693e9ae244060da4d1e026601b47b661
namtuthien <leducmanh123kt@gmail.com>
2024-04-02 08:54:33 +0700

    add: line TL787

--- Lab1/lines/index.js
+++ Lab1/lines/index.js
@@ -1,2 +1,3 @@
-export { default as QLAH1 } from "./QL-AH1.js"
-export { default as DT825 } from "./DT.825.js"
+export { default as QLAH1 } from "./QL-AH1.js";
+export { default as DT825 } from "./DT.825.js";
+export { default as TL787 } from "./TL787.js";
