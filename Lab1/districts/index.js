57561f9fe5dab9640eb139faecc95de67a7da450
seosau <seosausau@gmail.com>
2024-04-03 22:15:03 +0700

    add Go Dau and Duong Minh Chau

--- Lab1/districts/index.js
+++ Lab1/districts/index.js
@@ -2,9 +2,13 @@ import { BenCau } from "./BenCau.js";
 import { TrangBang } from "./TrangBang.js";
 import { ChauThanh } from "./ChauThanh.js";
 import { HoaThanh } from "./HoaThanh.js";
+import { GoDau } from "./GoDau.js";
+import { DuongMinhChau } from "./DuongMinhChau.js";
 var point_template_area = {
   title: "{name}",
-  content: "Diện tích: <b>{area} km<sup>2</sup></b>.<br>" + "Dân số: <b>{population} người</b>.",
+  content:
+    "Diện tích: <b>{area} km<sup>2</sup></b>.<br>" +
+    "Dân số: <b>{population} người</b>.",
 };
 
 const districts = [
@@ -65,6 +69,38 @@ const districts = [
     symbol: {
       type: "simple-fill",
       color: [32, 247, 100, 0.4],
+      outline: {
+        color: [255, 255, 255],
+        width: 1,
+      },
+    },
+    popupTemplate: point_template_area,
+  },
+  {
+    type: "polygon",
+    rings: GoDau,
+    name: "Huyện Gò Dầu",
+    population: 158711,
+    area: 260,
+    symbol: {
+      type: "simple-fill",
+      color: [32, 247, 50, 0.4],
+      outline: {
+        color: [255, 255, 255],
+        width: 1,
+      },
+    },
+    popupTemplate: point_template_area,
+  },
+  {
+    type: "polygon",
+    rings: DuongMinhChau,
+    name: "Huyện Dương Minh Châu",
+    population: 123792,
+    area: 435.6,
+    symbol: {
+      type: "simple-fill",
+      color: [32, 200, 70, 0.4],
       outline: {
         color: [255, 255, 255],
         width: 1,
