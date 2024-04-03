92ea1ea27fa64c864f736657f5ca496a05534d91
namtuthien <leducmanh123kt@gmail.com>
2024-04-03 09:51:57 +0700

    add: line QL22B

--- Lab1/lines/index.js
+++ Lab1/lines/index.js
@@ -1,3 +1,4 @@
 export { default as QLAH1 } from "./QL-AH1.js";
 export { default as DT825 } from "./DT.825.js";
 export { default as TL787 } from "./TL787.js";
+export { default as QL22B } from "./QL22B.js";
