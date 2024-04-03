d871ba1d900cf520ceeab6be77b7262307f642b7
Truong <truonghs2903@gmail.com>
2024-04-03 20:34:10 +0700

    add: Chau Thanh , Tan Chau district

--- Lab1/districts/index.js
+++ Lab1/districts/index.js
@@ -1,45 +1,77 @@
 import { BenCau } from "./BenCau.js";
 import { TrangBang } from "./TrangBang.js";
-
+import { ChauThanh } from "./ChauThanh.js";
+import { HoaThanh } from "./HoaThanh.js";
 var point_template_area = {
-    title: "{name}",
-    content: "Diện tích: <b>{area} km<sup>2</sup></b>.<br>" +
-        "Dân số: <b>{population} người</b>."
+  title: "{name}",
+  content: "Diện tích: <b>{area} km<sup>2</sup></b>.<br>" + "Dân số: <b>{population} người</b>.",
 };
 
 const districts = [
-    {
-        type: "polygon",
-        rings: TrangBang,
-        name: "Huyện Trảng Bàng",
-        population: 161831,
-        area: 340.14,
-        symbol: {
-            type: "simple-fill",
-            color: [150, 75, 0, 0.4],
-            outline: {
-                color: [255, 255, 255],
-                width: 1,
-            },
-        },
-        popupTemplate: point_template_area
+  {
+    type: "polygon",
+    rings: TrangBang,
+    name: "Huyện Trảng Bàng",
+    population: 161831,
+    area: 340.14,
+    symbol: {
+      type: "simple-fill",
+      color: [150, 75, 0, 0.4],
+      outline: {
+        color: [255, 255, 255],
+        width: 1,
+      },
     },
-    {
-        type: "polygon",
-        rings: BenCau,
-        name: "Huyện Bến Cầu",
-        population: 69849,
-        area: 264,
-        symbol: {
-            type: "simple-fill",
-            color: [248, 152, 128, 0.4],
-            outline: {
-                color: [255, 255, 255],
-                width: 1,
-            },
-        },
-        popupTemplate: point_template_area
-    }
-]
+    popupTemplate: point_template_area,
+  },
+  {
+    type: "polygon",
+    rings: BenCau,
+    name: "Huyện Bến Cầu",
+    population: 69849,
+    area: 264,
+    symbol: {
+      type: "simple-fill",
+      color: [248, 152, 128, 0.4],
+      outline: {
+        color: [255, 255, 255],
+        width: 1,
+      },
+    },
+    popupTemplate: point_template_area,
+  },
+  {
+    type: "polygon",
+    rings: ChauThanh,
+    name: "Huyện  Châu Thành",
+    population: 140769,
+    area: 580.94,
+    symbol: {
+      type: "simple-fill",
+      color: [66, 135, 245, 0.4],
+      outline: {
+        color: [255, 255, 255],
+        width: 1,
+      },
+    },
+    popupTemplate: point_template_area,
+  },
+  {
+    type: "polygon",
+    rings: HoaThanh,
+    name: "Thị xã  Hòa Thành",
+    population: 152339,
+    area: 82.88,
+    symbol: {
+      type: "simple-fill",
+      color: [32, 247, 100, 0.4],
+      outline: {
+        color: [255, 255, 255],
+        width: 1,
+      },
+    },
+    popupTemplate: point_template_area,
+  },
+];
 
 export default districts;
