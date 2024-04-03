37a74615b22d5fb6933e0e2ea72af5e5f1d465f1
namtuthien <leducmanh123kt@gmail.com>
2024-04-03 13:59:51 +0700

    add: point schools

---
+++ Lab1/streets/index.js
@@ -0,0 +1,62 @@
+import QLAH1 from "./QL-AH1.js";
+import DT825 from "./DT.825.js";
+import TL787 from "./TL787.js";
+import QL22B from "./QL22B.js";
+
+var point_template_line = {
+  title: "{Name}",
+  content: "Đường <b>{Name} - {Location}</b>.",
+};
+
+const streets = [
+  {
+    type: "polyline",
+    paths: QLAH1,
+    symbol: {
+      type: "simple-line",
+      color: [226, 119, 40],
+      width: 2,
+    },
+    Name: "QL-AH1",
+    Location: "Tỉnh Tây Ninh",
+    popupTemplate: point_template_line,
+  },
+  {
+    type: "polyline",
+    paths: DT825,
+    symbol: {
+      type: "simple-line",
+      color: [111, 119, 2],
+      width: 2,
+    },
+    Name: "DT.825",
+    Location: "Tỉnh Tây Ninh",
+    popupTemplate: point_template_line,
+  },
+  {
+    type: "polyline",
+    paths: TL787,
+    symbol: {
+      type: "simple-line",
+      color: [50, 150, 80],
+      width: 2,
+    },
+    Name: "TL 787",
+    Location: "Tỉnh Tây Ninh",
+    popupTemplate: point_template_line,
+  },
+  {
+    type: "polyline",
+    paths: QL22B,
+    symbol: {
+      type: "simple-line",
+      color: [180, 40, 40],
+      width: 2,
+    },
+    Name: "QL 22B",
+    Location: "Tỉnh Tây Ninh",
+    popupTemplate: point_template_line,
+  },
+];
+
+export default streets;
