c4e7b04059789dea9f4d4981055547754e7dae62
QuocDuy03 <quocduyy.03@gmail.com>
2024-04-27 23:53:32 +0700

    add: nha cua Duy

--- Lab2/main.js
+++ Lab2/main.js
@@ -538,6 +538,34 @@ require([
           ],
         },
       },
+      {
+        value: "Cửa ra vào",
+        symbol: {
+          type: "polygon-3d",
+          symbolLayers: [
+            {
+              type: "extrude",
+              material: {
+                color: "gray",
+              },
+            },
+          ],
+        },
+      },
+      {
+        value: "Tòa nhà",
+        symbol: {
+          type: "polygon-3d",
+          symbolLayers: [
+            {
+              type: "extrude",
+              material: {
+                color: "white",
+              },
+            },
+          ],
+        },
+      },
     ],
     visualVariables: [heightVV],
   };
